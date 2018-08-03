a66bdcf7b58dd89f8b0634b07db4829b037c8ad7
nazlisiasi <33232847+nazlisiasi@users.noreply.github.com>
2018-08-03 11:06:30 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import pexpect
+import yaml
+
+
+input_file = 'vm_info.yaml'
+input_dict = yaml.load(open(input_file))
+
+print input_dict, len(input_dict['vm_details'])
+prompt = '\$'
+i = 0
+while i < len(input_dict['vm_details']):
+   print "login ids", input_dict['vm_details'][i]['mgmt_ip']
+   print "PROMPT IS", prompt
+   print "SSH TO ", input_dict['vm_details'][i]['mgmt_ip']
+   print "KEY ", input_dict['vm_details'][i]['key']
+   ssh_command = '/home/ec2-user/' + input_dict['vm_details'][i]['key'] + ' ' + input_dict['vm_details'][i]['mgmt_ip'] 
+   child = pexpect.spawn( 'ssh -i '+ ssh_command)
+   child.expect (prompt)
+   child.logfile = open("/home/ec2-user/results.txt", "w")
+   print child.before
+
+   j = 0
+   while  j < len(input_dict['vm_details'][i]['ping_ips']):
+      print "ping to ", input_dict['vm_details'][i]['ping_ips'][j]
+      child.sendline ('ping -c 3 ' + input_dict['vm_details'][i]['ping_ips'][j])
+      child.expect (prompt)
+      print child.before, child.after
+      print "req is done"
+
+      j += 1
+   child.close()
+   i += 1
+
+print "ALL REQUESTS ARE DONE!"
+print "PINGING ALL INTSNACES IS DONE"
